388b2eafb4697498949919ba364fe5a180572710
Gigagamer046 <HK2005Boy@gmail.com>
2016-06-14 16:15:49 +0800

    game upload

---
+++ game/js/index.js
@@ -0,0 +1,69 @@
+function playGame () {
+alert("In a distant land a young boy named Jeff went to da forest to look for some food");
+
+var question1 = prompt('You run into a monster! Do you run or do you fight?'); 
+
+if (question1 === "run") {alert('A:You run away and quickly escape!');}
+
+else {alert('B: you decide to stay and fight! What a stuipid choice you realize that you cannot win. You are such a loser then you walk away. PS YOU ARE SUCH A LOSER');}
+
+var question2 = prompt("Next, you find some weapons on the ground. Do you pick the sword or the sniper rifle without a scope?")
+
+if (question2 === "sword") {alert('B: You pick up the sword. WHY? What a stuipid choice.c');}
+
+else {alert('A: You pick up the sniper. and feel ready to 360 no-scope some people.')}
+
+var question_3 = prompt('You have a weapon do you want to explore or go home like a wuss?');
+
+if (question_3 === "explore") {alert('B: You decide to continue exploring. IDIOT');}
+
+else {alert('A: You decide to go home. You took the safe choice')}
+
+var question_4 = prompt('You run into the same monster do you run or do you fight?');
+
+if (question_4 === "fight") {alert('B: you decide to stay and fight! You are likey to die the beast scrapes your arm and gives you a tiny cut. You try to run home to your MOMMY');}
+
+else {alert('A: You run and save yourself')}
+
+var question_5 = prompt('as you are running you see a river. Do you try to swim or jump?');
+
+if (question_5 === "Swim") {alert('A: You swim you barely cross the river and the beast catches up');}
+
+else {alert('B: you jump and fail. You try and climb up the side of the river bank now the beat has caught up with you')}
+
+var question_6 = prompt('the beast lunges at you do you jump or do you duck');
+
+if (question_6 === "duck") {alert('B: You block the blow with a nearby duck. YOU ARE SUCH A HORRIBLE PERSON');}
+
+else {alert('A: You jump and do a 360 and dodge the blow you feel mlg and start humming the John Cena song')}
+
+var question_7 = prompt('The beast is still chasing you do you try and make a run for the village or do you hide?');
+
+if (question_7 === "Run") {alert('A: You try to make a run for it and pass a nearby sanic and then the beast is right behind you. You find another sniper rifle without a scope and 360 noscope him.');}
+
+else {alert('A: You hide then find another sniper rifle without a scope and 360 noscope him.')}
+
+var question_8 = prompt('Without the beast you feel safer do you go looking for the lost deal with it glasses or do you go home?');
+
+if (question_8 === "looking for the lost deal with it glasses") {alert('B: You journey for many years searching for the lost deal with it glasses. and you find some boy named William wearing them you ask if you can have them but William says deal wth it and you walk away crying.');}
+
+else {alert('B: you try and go home for like the 3rd time when you meet a boy named william, his pure MLG levels are so high that they ARE OVER NINETHOUSAND you walk away crying')}
+
+var question_9 = prompt('You find some doritos in the forst do you yolo and eat them or do you ebolo and not (Ps if you know Ryan Higa you would know that EBOLO stands for everybody only lives once at means instead of yolo yo live life to the safest)');
+
+if (question_9 === "Yolo") {alert('A: You eat the doritos and become a god');}
+
+else {alert('B: You try and ebolo but the pull of the doritos is to much and you eat it and become a god.')}
+
+var question_10 = prompt('You fly around as a god when you meet William him from below or do you try to fight him straight on');
+
+if (question_10 === "below") {alert('A: You charge at William and do you try and fly under him to strike  but he catches the blow and you are locked in combat');}
+
+else {alert('B: You fly at him from above and you are locked in combat. He knockes to the ground and you fly away scared')}
+
+ var question_11 = prompt('You are safe do you go home or have fun with your God powers');
+
+if (question_11 === "Go home") {alert('You go home the end PS. if you got mostly As you are MLG if you got mostly Bs you are lame and go die.');}
+
+else {alert('You fly around but then the dorito power runs out and you fall to your death. GAME OVER! PS. if you got mostly As you are MLG if you got mostly Bs you are lame and go die.')}
+}
